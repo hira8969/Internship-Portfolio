c4c1255a5762b6ac666a27a140f95062138b6c22
hira8969 <hk2493200@gmail.com>
2026-05-07 22:09:08 +0530

    updated project

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,5 +1,13 @@
 import { v2 as cloudinary } from 'cloudinary';
 
+export function isCloudinaryConfigured() {
+  return Boolean(
+    process.env.CLOUDINARY_CLOUD_NAME &&
+    process.env.CLOUDINARY_API_KEY &&
+    process.env.CLOUDINARY_API_SECRET
+  );
+}
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,7 +1,14 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { isCloudinaryConfigured } from '../config/cloudinary.js';
 import { uploadBuffer } from '../services/cloudinaryService.js';
 
 export const uploadAsset = asyncHandler(async (req, res) => {
+  if (!isCloudinaryConfigured()) {
+    const error = new Error('Cloudinary is not configured. Set CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, and CLOUDINARY_API_SECRET.');
+    error.statusCode = 503;
+    throw error;
+  }
+
   if (!req.file) {
     const error = new Error('No file uploaded');
     error.statusCode = 400;
